b612985b35d2acd63d392aba2f6f2c8425fe62c4
Richard <Richard@DESKTOP-B3TIODP>
2016-10-11 15:53:59 +0100

    Added initial clinical area selection

---
+++ SepsisTrust/SepsisTrust/ViewModels/SelectClinicalAreaPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using SepsisTrust.Model;
+
+namespace SepsisTrust.ViewModels
+{
+    public class SelectClinicalAreaPageViewModel : BindableBase
+    {
+        private ObservableCollection<ClinicalArea> _availableClinicalAreas = new ObservableCollection<ClinicalArea>();
+
+        public SelectClinicalAreaPageViewModel()
+        {
+            AvailableClinicalAreas.Add(new ClinicalArea
+            {
+                Name = "Emergency Department", RelevantGuidelineIdentifiers = {"ED-ADULT"}
+            });
+        }
+
+        public ObservableCollection<ClinicalArea> AvailableClinicalAreas
+        {
+            get { return _availableClinicalAreas; }
+            set { SetProperty(ref _availableClinicalAreas, value); }
+        }
+    }
+}
