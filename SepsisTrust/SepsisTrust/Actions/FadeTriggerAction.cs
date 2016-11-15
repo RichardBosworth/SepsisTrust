cda689b10a06a6c4d740732c08603451b173f64a
Richard <Richard@DESKTOP-B3TIODP>
2016-11-15 17:43:41 +0000

    Enabled selection of icons.

---
+++ SepsisTrust/SepsisTrust/Actions/FadeTriggerAction.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace SepsisTrust.Actions
+{
+    public class FadeTriggerAction : TriggerAction<VisualElement>
+    {
+        public FadeTriggerAction() { }
+
+        public double DesiredOpacity { set; get; }
+
+        protected override async void Invoke(VisualElement visual)
+        {
+            visual.FadeTo(DesiredOpacity, 250, Easing.SinInOut);
+        }
+    }
+}
