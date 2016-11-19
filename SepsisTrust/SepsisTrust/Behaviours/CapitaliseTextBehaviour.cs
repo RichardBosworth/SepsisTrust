204cfb9f08a1c27ddd3e4a77d7fc186d5a72844c
Richard <Richard@DESKTOP-B3TIODP>
2016-11-19 00:28:54 +0000

    Updated SummaryPage

--- SepsisTrust/SepsisTrust/Behaviours/CapitaliseTextBehaviour.cs
+++ SepsisTrust/SepsisTrust/Behaviours/CapitaliseTextBehaviour.cs
@@ -5,10 +5,9 @@ namespace SepsisTrust.Behaviours
 {
     public class CapitaliseTextBehaviour : Behavior<Label>
     {
-
         protected override void OnAttachedTo( Label bindable )
         {
-            bindable.Text = bindable.Text.Transform(To.UpperCase);
+            bindable.Text = bindable.Text?.Transform(To.UpperCase);
             base.OnAttachedTo(bindable);
         }
     }
