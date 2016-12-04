ac29407630c00053b7a6991b4aab93907bb56e97
Richard <Richard@DESKTOP-RT9DDBH>
2016-12-04 19:43:57 +0000

    Added class for Local CRUD operations with Azure Service. This has no content currently, and thus requires further work.

---
+++ AzureData/LocalAzureCrudService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace AzureData
+{
+    /// <summary>
+    /// Provides functionality to obtain and syncronise (storing locally on a device) the data in an Azure service.
+    /// </summary>
+    public class LocalAzureCRUDService : IAzureCRUDService
+    {
+        private readonly MobileServiceClient _mobileServiceClient;
+
+        public LocalAzureCRUDService(MobileServiceClient mobileServiceClient)
+        {
+            _mobileServiceClient = mobileServiceClient;
+        }
+
+
+        public Task<List<T>> GetAll<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> GetOne<T>(string identifier)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Create<T>(T data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update<T>(T data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete<T>(string identifier)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IMobileServiceTableQuery<T> CreateQuery<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<T>> ExecuteQuery<T>(IMobileServiceTableQuery<T> query)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- AzureData/StaticAzureService.cs
+++ AzureData/StaticAzureService.cs
@@ -21,5 +21,7 @@ namespace AzureData
                 IsInitialized = true;
             }
         }
+
+
     }
 }
