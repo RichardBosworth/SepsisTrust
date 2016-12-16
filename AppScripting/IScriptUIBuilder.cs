2d5102566ef4fa87d17069c9e9ec45cd5d028356
Richard <Richard@DESKTOP-RT9DDBH>
2016-12-16 16:40:30 +0000

    Added initial project for app scripting.

---
+++ AppScripting/IScriptUIBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppScripting
+{
+    /// <summary>
+    /// Provides functionality to allow scripts to build UI elements.
+    /// </summary>
+    interface IScriptUIBuilder
+    {
+        Label BuildLabel(string labelContent);
+        Entry BuildEntry();
+    }
+}
---
+++ AppScripting/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jint;
+using Jint.Parser.Ast;
+
+namespace AppScripting
+{
+    class Test
+    {
+        public Test()
+        {
+            Engine engine = new Engine(options => options.AllowClr());
+        }
+    }
+}
